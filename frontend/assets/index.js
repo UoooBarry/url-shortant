b18a20aa018160fa02a51d2df3978dd767e94673
Barry Huang <barry.h@shopperplus.com>
2021-03-26 16:23:55 +0800

    update template

--- frontend/assets/index.js
+++ frontend/assets/index.js
@@ -1,9 +1,10 @@
+const SERVER_ADDRESS = 'http://168.138.40.63:3000'
 document.getElementById("short-url-form").addEventListener("submit", (event) => {
   url = document.querySelector('#url').value;
 
   if(url){
     console.log(url);
-    fetch('http://168.138.40.63:3000/api/v0/urls/', {
+    fetch(`${SERVER_ADDRESS}/api/v0/urls/`, {
       method: 'POST',
       headers: {
         'Accept': 'application/json',
@@ -12,9 +13,13 @@ document.getElementById("short-url-form").addEventListener("submit", (event) => 
       body: JSON.stringify({url: url})
     })
     .then((response) => {
-      const content = response.json();
-      console.log(response);
-      document.getElementById('result').innerHTML = content.uuid
+      response.json()
+              .then((content) => {
+                const uuid = content.payload.uuid
+                const redirectUrl = `${SERVER_ADDRESS}/redirect/${uuid}`
+                document.querySelector('#result').innerHTML = redirectUrl
+                document.querySelector('#result').href = redirectUrl 
+              })
     })
     .catch((err) => {
       console.log(error)
